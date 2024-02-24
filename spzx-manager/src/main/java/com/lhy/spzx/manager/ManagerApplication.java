b07e54fcb253276b5e51d4142661dd560d65e391
hliu557 <2509841336@qq.com>
2024-02-24 12:57:29 +0800

    202402241257

---
+++ spzx-manager/src/main/java/com/lhy/spzx/manager/ManagerApplication.java
@@ -0,0 +1,11 @@
+package com.lhy.spzx.manager;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class ManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApplication.class,args);
+    }
+}
---
+++ spzx-manager/src/main/java/com/lhy/spzx/manager/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.lhy.spzx.manager.controller;
+
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "用户接口")
+@RestController
+@RequestMapping(value="/admin/system/index")
+public class IndexController {
+}
